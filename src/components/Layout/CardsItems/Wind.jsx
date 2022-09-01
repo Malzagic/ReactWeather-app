e58e7a567882352ecb922399a6925faa3264aaf6
Malzagic <przemek2142@gmail.com>
2022-09-01 21:26:05 +0200

    change card titles

--- src/components/Layout/CardsItems/Wind.jsx
+++ src/components/Layout/CardsItems/Wind.jsx
@@ -6,7 +6,7 @@ function Wind({wind}) {
     <div className='col-sm-6 col-md-4 col-lg-2 mt-0 mt-md-2 mt-lg-5 px-0 px-md-1 px-lg-2'>
       <div className="card">
         <div className="container text-center">
-          <h4 className="card-title mt-2">Temp max</h4>
+          <h4 className="card-title mt-2">Wind speed</h4>
           <div className="card-body p-0 d-flex align-items-center justify-content-center">
             <div className="card-text fs-1 fw-bold">
               <div>
